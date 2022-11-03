029497f988281b708a8f7874d002ac8386c37572
Josh Darling <71955043+Josh-Darling13@users.noreply.github.com>
2022-11-03 16:52:40 -0400

    added state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person';
 
-function App() {
-  return (
-    <div className="App">
-      <Person name="Spud" age="72"/>
-      <Person name="Nancy" age="27">Some text for example stuff </Person>
-      <Person name="Bonnie" age="30"> Probably Rotten</Person>
-    </div>
-  );
-}
+class App extends Component {
+
+  state = {
+    persons : [
+      {name: 'Max', age: 45},
+      {name: 'Nancy', age: 21},
+      {name: 'Bonnie', age: 25},
+    ]
+  }
+
+
+
+  render(){ 
+    return (
+      <div className="App">
+        <h1>bumping the button down</h1>
+        <button onClick={}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Some text for example stuff </Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> Probably Rotten</Person>
+      </div>
+    );
+}}
 
 export default App;
